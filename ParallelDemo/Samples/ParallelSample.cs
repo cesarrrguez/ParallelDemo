16e204eb7c485776c4412d2fa07d86d6685e3e33
César Rodríguez <cesar.rrguez@gmail.com>
2020-06-12 08:43:31 +0200

    Implement parallel loop for example

--- ParallelDemo/Samples/ParallelSample.cs
+++ ParallelDemo/Samples/ParallelSample.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ParallelDemo.Samples
 {
@@ -9,7 +11,23 @@ namespace ParallelDemo.Samples
 
         public void Run(int threads, int iterations)
         {
-            throw new NotImplementedException();
+            var rnd = new Random();
+
+            var result = Parallel.For(0, iterations, new ParallelOptions { MaxDegreeOfParallelism = threads }, (i, state) =>
+            {
+                var delay = rnd.Next(1000, 10000);
+
+                ConsoleUtil.PrintLine($"START \t\t iteration {i} \t\t Thread {Thread.CurrentThread.ManagedThreadId} \t\t Delay {delay}", ConsoleColor.Green);
+
+                Thread.Sleep(delay);
+
+                ConsoleUtil.PrintLine($"END \t\t iteration {i} \t\t Thread {Thread.CurrentThread.ManagedThreadId}", ConsoleColor.Red);
+            });
+
+            if (result.IsCompleted)
+            {
+                ConsoleUtil.PrintLine("\nAll iterations completed successfully", ConsoleColor.Green);
+            }
         }
     }
 }
